10f4966fac21058a0843965cf76a91dc00a40e9c
Eric Abides <ericxdu23@gmail.com>
2025-08-12 20:56:00 -0700

    enumerate chapters

--- bounce/src/Bounce.js
+++ bounce/src/Bounce.js
@@ -2,29 +2,27 @@ import Ball from './Ball.js';
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-const width = canvas.width;
-const height = canvas.height;
 
-let ball = new Ball(width / 3, height / 4, 1.5, 2.3, 12, "Blue");
+let ball = new Ball(canvas.width / 3, canvas.height / 4, 1.5, 2.3, 12, "Blue");
 
 function drawPattern (ctx) {
     ctx.beginPath();
-    ctx.rect(0, 0, width / 2, height / 2);
+    ctx.rect(0, 0, ctx.canvas.width / 2, ctx.canvas.height / 2);
     ctx.fillStyle = "Gray";
     ctx.fill();
     ctx.closePath();
     ctx.beginPath();
-    ctx.rect(width / 2, height / 2, width / 2, height / 2);
+    ctx.rect(ctx.canvas.width / 2, ctx.canvas.height / 2, ctx.canvas.width / 2, ctx.canvas.height / 2);
     ctx.fillStyle = "Gray";
     ctx.fill();
     ctx.closePath();
     ctx.beginPath();
-    ctx.rect(width / 2, 0, width / 2, height / 2);
+    ctx.rect(ctx.canvas.width / 2, 0, ctx.canvas.width / 2, ctx.canvas.height / 2);
     ctx.fillStyle = "White";
     ctx.fill();
     ctx.closePath();
     ctx.beginPath();
-    ctx.rect(0, height / 2, width / 2, height / 2);
+    ctx.rect(0, ctx.canvas.height / 2, ctx.canvas.width / 2, ctx.canvas.height / 2);
     ctx.fillStyle = "White";
     ctx.fill();
     ctx.closePath();
@@ -32,7 +30,7 @@ function drawPattern (ctx) {
 
 function draw () {
     drawPattern(ctx);
-    ball.move({x: 0, y: 0, width: width, height: height});
+    ball.move({x: 0, y: 0, width: canvas.width, height: canvas.height});
     ball.draw(ctx);
 }
 
